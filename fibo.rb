6fd6c67c7a58946005c421383ae035b3039c7579
Trystan Hendricks <dolkhound@gmx.com>
2014-02-28 22:44:36 -0500

    fibonacci output

---
+++ fibo.rb
@@ -0,0 +1,6 @@
+def fibonacci( i )
+  return  i  if ( 0..1 ).include? i
+  ( fibonacci( i - 1 ) + fibonacci( i - 2 ) )
+end                                                                                                                   
+                                                                                                                          
+puts fibonacci(10)                                                                                                             
